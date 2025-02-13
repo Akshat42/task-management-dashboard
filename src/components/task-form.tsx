c108f41565ee421feae610e903a93df1b9151ad8
Akshat42 <akshatdivya03@gmail.com>
2025-02-13 17:35:58 +0530

    fix: add error handeling to task-form

--- src/components/task-form.tsx
+++ src/components/task-form.tsx
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import { Label } from "./ui/label";
 import { Input } from "./ui/input";
 import { Textarea } from "./ui/textarea";
@@ -12,26 +11,31 @@ import {
 import { Button } from "./ui/button";
 import { Task } from "@/types/tasks";
 import useTaskContext from "@/hooks/use-task-context";
+import { useForm } from "react-hook-form";
 
 type TaskFormProps = {
   task?: Task;
   onClose: () => void;
 };
 
+interface TaskFromInput {
+  title: string;
+  description: string;
+  status: Task["status"];
+  dueDate: string;
+}
+
 function TaskForm({ task, onClose }: TaskFormProps) {
   const { addTask, editTask } = useTaskContext();
-  const [title, setTitle] = useState(task?.title || "");
-  const [description, setDescription] = useState(task?.description || "");
-  const [status, setStatus] = useState<Task["status"]>(
-    task?.status || "Pending"
-  );
-  const [dueDate, setDueDate] = useState(task?.dueDate || "");
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    if (!title || !dueDate) return;
-
-    const newTask = { title, description, status, dueDate };
+  const submitForm = (data: TaskFromInput) => {
+    if (!data.title || !data.dueDate) return;
+    const newTask = {
+      title: data.title,
+      description: data.description,
+      status: data.status,
+      dueDate: data.dueDate,
+    };
     if (task) {
       editTask(task.id, newTask);
     } else {
@@ -40,32 +44,43 @@ function TaskForm({ task, onClose }: TaskFormProps) {
     onClose();
   };
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+  } = useForm<TaskFromInput>();
+
+  const handleSelectChange = (value: Task["status"]) => {
+    setValue("status", value, { shouldValidate: true });
+  };
+
   return (
-    <form onSubmit={handleSubmit} className="space-y-4">
+    <form onSubmit={handleSubmit(submitForm)} className="space-y-4">
       <div>
         <Label htmlFor="title">Title</Label>
-        <Input
-          id="title"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          required
-        />
+        <Input id="title" {...register("title", { required: true })} />
+        {errors.title && (
+          <span className="text-sm text-red-400">Title is required</span>
+        )}
       </div>
       <div>
         <Label htmlFor="description">Description</Label>
         <Textarea
           id="description"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
+          {...register("description", { required: true })}
         />
+        {errors.description && (
+          <span className="text-sm text-red-400">Description is required</span>
+        )}
       </div>
       <div>
         <Label htmlFor="status">Status</Label>
         <Select
-          value={status}
-          onValueChange={(value) => setStatus(value as Task["status"])}
+          {...register("status", { required: true })}
+          onValueChange={handleSelectChange}
         >
-          <SelectTrigger>
+          <SelectTrigger {...register("status", { required: true })}>
             <SelectValue placeholder="Select status" />
           </SelectTrigger>
           <SelectContent>
@@ -74,16 +89,20 @@ function TaskForm({ task, onClose }: TaskFormProps) {
             <SelectItem value="Completed">Completed</SelectItem>
           </SelectContent>
         </Select>
+        {errors.status && (
+          <span className="text-sm text-red-400">Status is required</span>
+        )}
       </div>
       <div>
         <Label htmlFor="dueDate">Due Date</Label>
         <Input
           id="dueDate"
           type="date"
-          value={dueDate}
-          onChange={(e) => setDueDate(e.target.value)}
-          required
+          {...register("dueDate", { required: true })}
         />
+        {errors.dueDate && (
+          <span className="text-sm text-red-400">Due Date is required</span>
+        )}
       </div>
       <Button type="submit">{task ? "Update Task" : "Add Task"}</Button>
     </form>
